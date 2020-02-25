9f9f18a6c785add4ea40de0b6ee7fe422a7d7f79
OmarPi <olseda20@bath.ac.uk>
2020-02-25 11:58:41 +0000

    saliency model working on the pi

--- saliency.py
+++ saliency.py
@@ -1,35 +1,49 @@
 import cv2
-# import RGBCam
+import RGBCam
+from time import time
 
 ##import image
-# from RGBCam import PiCam().frameCapture() as RGB
-# from IRCam import getImage() as IR 
-cap = cv2.VideoCapture(0)
-if not (cap.isOpened()):
-    print("Could not open video device")
+RGBCam = RGBCam.PiCam()
 
-def getRGB():    
-    ret, frame = cap.read()
-    return frame 
+# from IRCam import getImage() as IR 
+
+# cap = cv2.VideoCapture(0)
+# if not (cap.isOpened()):
+#     print("Could not open video device")
+
+# def getRGB():    
+#     ret, frame = cap.read()
+#     return frame 
 
 ##Saleincy analysis
 ##find ROI in image
 
 cv2.namedWindow("RGB",cv2.WINDOW_NORMAL)
 
+def visImageProc():
+      #Get Visual Image
+      RGB = RGBCam.frameCapture()
+      return RGB
+t=0
+t0 = 0
 while True:
-    RGB = getRGB()
-    saliency = cv2.saliency.StaticSaliencyFineGrained_create()    
-    (success, saliencyMap) = saliency.computeSaliency(RGB)
-    threshMap = cv2.threshold(saliencyMap.astype("uint8"),100,255,
-    cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
-    cv2.imshow("RGB",RGB)
-    cv2.imshow("threshMap",threshMap)
-    cv2.imshow("SalMap",saliencyMap)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+      t = time()
+      print("fps:",1/(t-t0))
+      t0 = time()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.waitKey(1)
+      RGB = visImageProc()
+      # saliency = cv2.saliency.StaticSaliencyFineGrained_create()    
+      saliency = cv2.saliency.StaticSaliencySpectralResidual_create()    
+      (success, saliencyMap) = saliency.computeSaliency(RGB)
+      # threshMap = cv2.threshold(saliencyMap.astype("uint8"),100,255,
+      # cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
+      cv2.imshow("RGB",RGB)
+      # cv2.imshow("threshMap",threshMap)
+      cv2.imshow("SalMap",saliencyMap)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+      cv2.waitKey(1)
     
