fb71f5d31e4051b23c713fd475976c8201eb0009
Omro <olseda20@bath.ac.uk>
2020-02-25 11:11:55 +0000

    Further work on saliency code

--- saliency.py
+++ saliency.py
@@ -4,11 +4,11 @@ import cv2
 ##import image
 # from RGBCam import PiCam().frameCapture() as RGB
 # from IRCam import getImage() as IR 
+cap = cv2.VideoCapture(0)
+if not (cap.isOpened()):
+    print("Could not open video device")
 
-def getRGB():
-    cap = cv2.VideoCapture(0)
-    if not (cap.isOpened()):
-        print("Could not open video device")
+def getRGB():    
     ret, frame = cap.read()
     return frame 
 
@@ -19,11 +19,17 @@ cv2.namedWindow("RGB",cv2.WINDOW_NORMAL)
 
 while True:
     RGB = getRGB()
-    # saliency = cv2.saliency.StaticSaliencyFineGrained_create()    
-    # (success, saliencyMap) = saliency.computeSaliency(RGB)
-    # threshMap = cv2.threshold(saleincyMap.astype("uint8"),0,255,
-    # cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
+    saliency = cv2.saliency.StaticSaliencyFineGrained_create()    
+    (success, saliencyMap) = saliency.computeSaliency(RGB)
+    threshMap = cv2.threshold(saliencyMap.astype("uint8"),100,255,
+    cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
     cv2.imshow("RGB",RGB)
-    # cv2.imshow("threshMap",threshMap)
+    cv2.imshow("threshMap",threshMap)
+    cv2.imshow("SalMap",saliencyMap)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #    break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.waitKey(1)
+    
